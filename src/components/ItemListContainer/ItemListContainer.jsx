3f2299c8f6c5ea262cd697dfe501a47d9d69ad95
Tomás Lazarte <tomaslazarte.dev@gmail.com>
2022-09-07 23:52:16 -0300

    pre-entrega proyecto

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -27,7 +27,7 @@ export const ItemListContainer = () => {
                 cargando?
                     <div>Cargando...</div>
                 :
-                    <ItemList items={producto}/>
+                    <ItemList items = {producto}/>
             }
         </div>
     );
